60fde4d14ee77e3aecc017847d8de4d4c51bfe27
Tymofii Maksymenko <tim.off.max@gmail.com>
2022-02-11 17:13:14 +0200

    Feature: Pre-release refactoring. – Use the unified way of getting absolute path.

--- src/Model/Config/StoreConfigReader.php
+++ src/Model/Config/StoreConfigReader.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 namespace Montikids\MagentoCliUtil\Model\Config;
 
 use Montikids\MagentoCliUtil\Enum\Config\StoreConfigInterface;
+use Montikids\MagentoCliUtil\Enum\FileDirInterface;
 
 /**
  * Magento store config environment config reader
  */
 class StoreConfigReader extends AbstractYamlReader
 {
-    /**
-     * @var string
-     */
-    protected const CONFIG_DIR_PATH = StoreConfigInterface::PATH_DIR;
+    protected const CONFIG_DIR_PATH = FileDirInterface::DIR_UTIL_CONFIG_STORE_CONFIG;
     protected const CONFIG_SECTIONS = StoreConfigInterface::ROOT_SECTIONS;
 }
