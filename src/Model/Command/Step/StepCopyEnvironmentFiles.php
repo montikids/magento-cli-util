8472e6f18460508c3c47f23470c744f1cc85d19c
Tymofii Maksymenko <tim.off.max@gmail.com>
2022-02-15 13:30:37 +0200

    Feature: Pre-release refactoring. – Refactor code duplication cases. Warn user about importance of modifying config samples.

--- src/Model/Command/Step/StepCopyEnvironmentFiles.php
+++ src/Model/Command/Step/StepCopyEnvironmentFiles.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Montikids\MagentoCliUtil\Model\Command\Step;
 
 use Montikids\MagentoCliUtil\Enum\FileDirInterface;
-use Montikids\MagentoCliUtil\Enum\Magento\EnvFileInterface;
 use Montikids\MagentoCliUtil\Model\Command\OutputFormatTrait;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
@@ -16,6 +15,11 @@ use Symfony\Component\Filesystem\Filesystem;
 class StepCopyEnvironmentFiles
 {
     use OutputFormatTrait;
+
+    /**
+     * @var string[]
+     */
+    private $newConfigs = [];
 
     /**
      * @var Filesystem
@@ -38,8 +42,11 @@ class StepCopyEnvironmentFiles
     public function execute(string $envType, OutputInterface $output): void
     {
         $this->printTitle("Copying files...", $output);
+
         $this->copyBasicFiles($output);
         $this->copyEnvFiles($envType, $output);
+
+        $this->printNewConfigsWarning($output);
     }
 
     /**
@@ -77,28 +84,51 @@ class StepCopyEnvironmentFiles
             FileDirInterface::FILE_NAME_CONFIG_BASE
         );
 
+        $localConfig1Src = sprintf(
+            '%s/%s',
+            FileDirInterface::DIR_PKG_CONFIG_SAMPLE_ANONYMIZE,
+            FileDirInterface::FILE_NAME_CONFIG_LOCAL
+        );
+        $localConfig1Dst = sprintf(
+            '%s/%s',
+            FileDirInterface::DIR_UTIL_CONFIG_ANONYMIZE,
+            FileDirInterface::FILE_NAME_CONFIG_LOCAL
+        );
+
+        $localConfig2Src = sprintf(
+            '%s/%s',
+            FileDirInterface::DIR_PKG_CONFIG_SAMPLE_STORE_CONFIG,
+            FileDirInterface::FILE_NAME_CONFIG_LOCAL
+        );
+        $localConfig2Dst = sprintf(
+            '%s/%s',
+            FileDirInterface::DIR_UTIL_CONFIG_STORE_CONFIG,
+            FileDirInterface::FILE_NAME_CONFIG_LOCAL
+        );
+
         $this->filesystem->mirror($samplesSrc, $samplesDst, null, ['override' => true, 'copy_on_windows' => true]);
         $this->printCopyResult($samplesDst, false, $output);
 
-        if (false === $this->filesystem->exists($gitIgnoreDst)) {
-            $this->filesystem->copy($gitIgnoreSrc, $gitIgnoreDst);
-            $this->printCopyResult($gitIgnoreDst, true, $output);
-        } else {
-            $this->printSkipResult($gitIgnoreDst, $output);
+        $this->copyFileIfNotExist($gitIgnoreSrc, $gitIgnoreDst, true, $output);
+
+        if (true === $this->copyFileIfNotExist($baseConfig1Src, $baseConfig1Dst, true, $output)) {
+            $this->newConfigs[] = $baseConfig1Dst;
         }
 
-        if (false === $this->filesystem->exists($baseConfig1Dst)) {
-            $this->filesystem->copy($baseConfig1Src, $baseConfig1Dst);
-            $this->printCopyResult($baseConfig1Dst, true, $output);
-        } else {
-            $this->printSkipResult($baseConfig1Dst, $output);
+        if (true === $this->copyFileIfNotExist($baseConfig1Src, $baseConfig1Dst, true, $output)) {
+            $this->newConfigs[] = $baseConfig1Dst;
         }
 
-        if (false === $this->filesystem->exists($baseConfig2Dst)) {
-            $this->filesystem->copy($baseConfig2Src, $baseConfig2Dst);
-            $this->printCopyResult($baseConfig2Dst, true, $output);
-        } else {
-            $this->printSkipResult($baseConfig2Dst, $output);
+        if (true === $this->copyFileIfNotExist($baseConfig2Src, $baseConfig2Dst, true, $output)) {
+            $this->newConfigs[] = $baseConfig2Dst;
+        }
+
+        if (true === $this->copyFileIfNotExist($localConfig1Src, $localConfig1Dst, false, $output)) {
+            $this->newConfigs[] = $localConfig1Dst;
+        }
+
+        if (true === $this->copyFileIfNotExist($localConfig2Src, $localConfig2Dst, false, $output)) {
+            $this->newConfigs[] = $localConfig2Dst;
         }
     }
 
@@ -111,8 +141,6 @@ class StepCopyEnvironmentFiles
      */
     private function copyEnvFiles(string $envType, OutputInterface $output): void
     {
-        $isUnderVcs = ($envType !== EnvFileInterface::MK_CLI_UTIL_ENVIRONMENT_LOCAL);
-
         $envConfig1Src = sprintf(
             '%s/%s',
             FileDirInterface::DIR_PKG_CONFIG_SAMPLE_ANONYMIZE,
@@ -135,19 +163,36 @@ class StepCopyEnvironmentFiles
             "{$envType}.yml"
         );
 
-        if (false === $this->filesystem->exists($envConfig1Dst)) {
-            $this->filesystem->copy($envConfig1Src, $envConfig1Dst);
-            $this->printCopyResult($envConfig1Dst, $isUnderVcs, $output);
-        } else {
-            $this->printSkipResult($envConfig1Dst, $output);
+        if (true === $this->copyFileIfNotExist($envConfig1Src, $envConfig1Dst, true, $output)) {
+            $this->newConfigs[] = $envConfig1Dst;
         }
 
-        if (false === $this->filesystem->exists($envConfig2Dst)) {
-            $this->filesystem->copy($envConfig2Src, $envConfig2Dst);
-            $this->printCopyResult($envConfig2Dst, $isUnderVcs, $output);
-        } else {
-            $this->printSkipResult($envConfig2Dst, $output);
+        if (true === $this->copyFileIfNotExist($envConfig2Src, $envConfig2Dst, true, $output)) {
+            $this->newConfigs[] = $envConfig2Dst;
         }
+    }
+
+    /**
+     * @param string $src
+     * @param string $dst
+     * @param bool $underVcs
+     * @param OutputInterface $output
+     * @return bool
+     */
+    private function copyFileIfNotExist(string $src, string $dst, bool $underVcs, OutputInterface $output): bool
+    {
+        $result = false;
+
+        if (false === $this->filesystem->exists($dst)) {
+            $this->filesystem->copy($src, $dst);
+            $this->printCopyResult($dst, $underVcs, $output);
+
+            $result = true;
+        } else {
+            $this->printSkipResult($dst, $output);
+        }
+
+        return $result;
     }
 
     /**
@@ -165,8 +210,7 @@ class StepCopyEnvironmentFiles
         if (true === $isUnderVcs) {
             $this->printSecondary("Don't forget to commit the file to your repository!", $output);
         } else {
-            $this->printError("It's only a sample, don't forget to modify it!", $output);
-            $this->printSecondary("Also, it's ignored by VCS by default. Feel free to change it.", $output);
+            $this->printSecondary("It's ignored by VCS by default. Feel free to change it.", $output);
         }
 
         $this->printSeparatorSmall($output);
@@ -183,6 +227,24 @@ class StepCopyEnvironmentFiles
 
         $this->printSecondary("- SKIPPED (already exists): {$relativePath}", $output);
         $this->printSeparatorSmall($output);
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @return void
+     */
+    private function printNewConfigsWarning(OutputInterface $output): void
+    {
+        if (count($this->newConfigs) > 1) {
+            $this->printPrimary("The following new configs were initialized:", $output);
+
+            foreach ($this->newConfigs as $path) {
+                $relativePath = $this->getRelativePath($path);
+                $this->printSecondary("- {$relativePath}", $output);
+            }
+
+            $this->printError("That's only samples. Please, revise them and modify. Don't use them 'as is'!", $output);
+        }
     }
 
     /**
